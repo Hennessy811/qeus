58548b696fa726d81e8ba151ba7f5a3b01bc3261
Hennessy811 <mitia2022@gmail.com>
2018-09-25 15:25:06 +0300

    msg

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -21,10 +21,10 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = "An unknown error occurred!";
-        if (error.error.error.message) {
-          errorMessage = error.error.error.message;
+        if (error.error.message) {
+          errorMessage = error.error.message;
         }
-        this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
+        // this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
         // this.errorService.throwError(errorMessage);
         return throwError(error);
       })
